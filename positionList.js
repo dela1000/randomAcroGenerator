aced7573350ec1cfbc622b8e990b152bcce586be
Daniel <dela1000@gmail.com>
2015-11-03 19:32:59 -0800

    "Positions added, Difficulty rating logic"

--- positionList.js
+++ positionList.js
@@ -4,159 +4,146 @@ var positionList = [
 				"difficulty": "Beginner",
 				"on": "Hands and Feet",
 				"inverted": false,
-				"rating": 1.0,
-				
+				"rating": 1.0	
 			},
 			{
 				"name": "Throne",
 				"difficulty": "Beginner",
 				"on": "Hands and Feet",
 				"inverted": false,
-				"rating": 1.0,
-				
+				"rating": 1.0
 			},
 			{
 				"name": "Straddle Throne",
 				"difficulty": "Beginner",
 				"on": "Feet",
 				"inverted": false,
-				"rating": 2.0,
-				
+				"rating": 2.0
 			},
 			{
 				"name": "Vishnu\'s Couch",
 				"difficulty": "Beginner",
 				"on": "Feet",
 				"inverted": false,
-				"rating": 1.0,
-				
+				"rating": 1.0
 			},
 			{
 				"name": "High-Flying Whale",
 				"difficulty": "Beginner",
 				"on": "Hands and Feet",
 				"inverted": false,
-				"rating": 1.0,
-				
+				"rating": 1.0
 			},
 			{
 				"name": "Side Star",
 				"difficulty": "Intermediate",
 				"on": "Hands and Feet",
 				"inverted": false,
-				"rating": 2.0,
-				
+				"rating": 2.0
 			},
 			{
 				"name": "Star",
 				"difficulty": "Advanced",
 				"on": "Hands and Feet",
 				"inverted": true,
-				"rating": 3.0,
-				
+				"rating": 3.0
 			},
 			{
 				"name": "Shoulder Stand",
 				"difficulty": "Advanced",
 				"on": "Hands and Feet",
 				"inverted": false,
-				"rating": 4.0,
-				
+				"rating": 4.0
 			},
 			{
 				"name": "Back Bird",
 				"difficulty": "Intermediate",
 				"on": "Feet",
 				"inverted": false,
-				"rating": 2.0,
-				
+				"rating": 2.0
 			},
 			{
 				"name": "Straddle Bat",
 				"difficulty": "Intermediate",
 				"on": "Feet",
 				"inverted": true,
-				"rating": 2.0,
-				
+				"rating": 2.0
 			},
 			{
 				"name": "Floating Poshi",
 				"difficulty": "Advanced",
 				"on": "Hands",
 				"inverted": true,
-				"rating": 4.0,
-				
+				"rating": 4.0
 			},
 			{
 				"name": "Folded Leaf",
 				"difficulty": "Beginner",
 				"on": "Feet",
 				"inverted": true,
-				"rating": 1.0,
-				
+				"rating": 1.0
 			},
 			{
 				"name": "Foot-to-Hand",
 				"difficulty": "Advanced",
 				"on": "Hands",
 				"inverted": false,
-				"rating": 4.0,
-				
+				"rating": 4.0
 			},
 			{
 				"name": "Foot-to-Foot",
 				"difficulty": "Advanced",
 				"on": "Feet",
 				"inverted": false,
-				"rating": 4.0,
-				
+				"rating": 4.0
 			},
 			{
 				"name": "Hand-to-Hand",
 				"difficulty": "Advanced",
 				"on": "Hands",
 				"inverted": true,
-				"rating": 4.0,
-				
+				"rating": 4.0
 			},
 			{
 				"name": "Bat",
 				"difficulty": "Beginner",
 				"on": "Feet",
 				"inverted": true,
-				"rating": 1.0,
-				
+				"rating": 1.0
 			},
 			{
 				"name": "Couch",
 				"difficulty": "Intermediate",
 				"on": "Feet",
 				"inverted": false,
-				"rating": 2.0,
-				
+				"rating": 2.0
 			},
 			{
 				"name": "Reverse Star",
 				"difficulty": "Advanced",
 				"on": "Feet",
 				"inverted": true,
-				"rating": 5.0,
-				
+				"rating": 5.0
 			},
 			{
 				"name": "Tittibhasana",
 				"difficulty": "Advanced",
 				"on": "Hands",
 				"inverted": false,
-				"rating": 4.0,
-				
+				"rating": 4.0
 			},
 			{
 				"name": "Floating Camel",
 				"difficulty": "Advanced",
 				"on": "Hands",
 				"inverted": false,
-				"rating": 4.0,
-				
+				"rating": 4.0
+			},
+			{
+				"name": "Croc",
+				"difficulty": "Advanced",
+				"on": "Hands",
+				"inverted": false,
+				"rating": 4.0
 			}
 ];
